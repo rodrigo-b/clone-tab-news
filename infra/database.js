cc7d7a9a3d7113e1af415e651afe7bbbbd49237c
rodrigo-b <rodrigobrito.ciencia@gmail.com>
2024-04-27 19:55:34 +0000

    database query secutiry inclusion

--- infra/database.js
+++ infra/database.js
@@ -4,16 +4,21 @@ async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
   });
 
   await client.connect();
 
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -12,9 +12,11 @@ async function status(request, response) {
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const databaseOpenedConnectionsResult = await database.query(
-    "SELECT count(*)::int FROM pg_stat_activity where datname = 'local_db';",
-  );
+  var databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity where datname = $1",
+    values: [databaseName],
+  });
 
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
