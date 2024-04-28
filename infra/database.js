85ae57d10b3f9636639b554eb0e1fab256458cfe
rodrigo-b <rodrigobrito.ciencia@gmail.com>
2024-04-28 14:38:20 +0000

    ssl configuration inclusiion for database

--- infra/database.js
+++ infra/database.js
@@ -7,11 +7,11 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
-  await client.connect();
-
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
