52ba9901a15ad6be14117e9647c9a4895bb47071
rodrigo-b <rodrigobrito.ciencia@gmail.com>
2024-06-15 18:27:55 +0000

    Migrations test

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,17 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("Get to /api/v1/status should return 200", async () => {
+  database.query("select 1 + 1");
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+});
