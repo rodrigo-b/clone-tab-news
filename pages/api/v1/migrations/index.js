ad4b5563d3aad56a5df05144f2d042bbf05ddcf4
rodrigo-b <rodrigobrito.ciencia@gmail.com>
2024-06-16 18:28:21 +0000

    progress check

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,25 +1,26 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
   if (request.method === "GET") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
+    const migrations = await migrationRunner(defaultMigrationOptions);
     return response.status(200).json(migrations);
   } else if (request.method === "POST") {
     const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+      ...defaultMigrationOptions,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     });
     return response.status(200).json(migrations);
   }
