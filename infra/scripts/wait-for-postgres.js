e0741cb8868be13400466d515d17c6bc31df288c
rodrigo-b <rodrigobrito.ciencia@gmail.com>
2025-02-04 23:30:06 +0000

    adds `wait-for-postgres.js`

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,18 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+
+    console.log("\n 🪄 Postgres está pronto e aceitando conexões!\n");
+  }
+}
+
+process.stdout.write("\n\n‼️ Aguardando Postgres aceitar conexões");
+checkPostgres();
