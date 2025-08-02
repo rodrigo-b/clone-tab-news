02abc87ff0e9cf7fb365774616dc16d6345ac9cc
rodrigo-b <rodrigobrito.ciencia@gmail.com>
2025-08-02 19:02:20 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,12 +6,16 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("Get to /api/v1/status should return 200", async () => {
-  database.query("select 1 + 1");
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
-  const responseBody = await response.json();
-  console.log(responseBody);
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Running pending migrations", async () => {
+      database.query("select 1 + 1");
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
+      const responseBody = await response.json();
+      console.log(responseBody);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
